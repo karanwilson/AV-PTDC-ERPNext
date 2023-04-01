e32c1aac6257460f6cc10189c1f7e91d290f3ade
karanwilson <karanav@vivaldi.net>
2023-04-01 10:54:01 +0000

    In 'PT Purchase Receipt':- made the fields 'Currency' and 'Accepted Qty' mandatory
    In the report file pa_contribution_custom.js :-
    removed the from_date and to_date fields
    added a blank field in the months filter - in order to reset the report data
    
    In the report file pa_contribution_custom.py :-
    added the logic and sql querry for filtering data based on both month and year, or only month or only year

--- ptdc_av/ptdc_av/report/pa_contribution_custom/pa_contribution_custom.js
+++ ptdc_av/ptdc_av/report/pa_contribution_custom/pa_contribution_custom.js
@@ -4,25 +4,26 @@
 
 frappe.query_reports["PA Contribution Custom"] = {
 	"filters": [
+		/*
 		{
 			"fieldname": "from_date",
 			"label": __("From Date"),
 			"fieldtype": "Date",
 			"width": "60px"
 		},
-
 		{
 			"fieldname": "to_date",
 			"label": __("To Date"),
 			"fieldtype": "Date",
 			"width": "60px"
 		},
+		*/
 
 		{
 			"fieldname": "month",
 			"label": __("Month"),
 			"fieldtype": "Select",
-			"options": ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+			"options": ["", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
 			"width": "60px"
 		},
 
--- ptdc_av/ptdc_av/report/pa_contribution_custom/pa_contribution_custom.py
+++ ptdc_av/ptdc_av/report/pa_contribution_custom/pa_contribution_custom.py
@@ -59,9 +59,27 @@ def get_columns():
 	]
 
 def get_data(filters):
-	if filters.month:
+	if filters.month and filters.year_yyyy:
+		report_month_year = frappe.db.sql(
+			"""select `tabPayment Entry`.party_name, tabAddress.address_line1, `tabPayment Entry`.paid_amount,
+			`tabPayment Entry`.remarks, `tabPayment Entry`.posting_date
+			from `tabPayment Entry`
+			join `tabDynamic Link`
+			on `tabPayment Entry`.party = `tabDynamic Link`.link_name
+			join tabAddress
+			on `tabDynamic Link`.parent = tabAddress.name
+			where monthname(`tabPayment Entry`.posting_date)=%(month)s and year(`tabPayment Entry`.posting_date)=%(year)s""",
+			{
+				'month': filters.month,
+				'year' : filters.year_yyyy
+			},
+			as_dict=True,
+		)
+		return report_month_year
+
+	elif filters.month:
 		report_monthly = frappe.db.sql(
-			"""select `tabPayment Entry`.party_name, tabAddress.address_line1, `tabPayment Entry`.paid_amount, 
+			"""select `tabPayment Entry`.party_name, tabAddress.address_line1, `tabPayment Entry`.paid_amount,
 			`tabPayment Entry`.remarks, `tabPayment Entry`.posting_date
 			from `tabPayment Entry`
 			join `tabDynamic Link`
@@ -73,3 +91,18 @@ def get_data(filters):
 			as_dict=True,
 		)
 		return report_monthly
+
+	elif filters.year_yyyy:
+		report_yearly = frappe.db.sql(
+			"""select `tabPayment Entry`.party_name, tabAddress.address_line1, `tabPayment Entry`.paid_amount,
+			`tabPayment Entry`.remarks, `tabPayment Entry`.posting_date
+			from `tabPayment Entry`
+			join `tabDynamic Link`
+			on `tabPayment Entry`.party = `tabDynamic Link`.link_name
+			join tabAddress
+			on `tabDynamic Link`.parent = tabAddress.name
+			where year(`tabPayment Entry`.posting_date)=%s""",
+			filters.year_yyyy,
+			as_dict=True,
+		)
+		return report_yearly
